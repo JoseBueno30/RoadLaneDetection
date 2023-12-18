a370b4b4a9198597a2a7020e3c15192b2cae05bc
José Ángel Bueno Ruiz <jabr3830@gmail.com>
2023-12-18 14:23:25 +0100

    1.0

--- Main.py
+++ Main.py
@@ -1,14 +1,10 @@
 import numpy as np
 import cv2
 import time
-import matplotlib
-import matplotlib.pyplot as plt
 import threading
 import Lane
 import LaneFinder
 import PerspectiveTransformation
-import PreProcessing
-
 
 def drawCurvature(frame, left_aperture, right_aperture):
     aperture = None
@@ -16,7 +12,6 @@ def drawCurvature(frame, left_aperture, right_aperture):
         aperture = left_aperture
     else:
         aperture = right_aperture
-    print(abs(aperture))
 
     if abs(aperture) <= 0.00003:
         direction = "Straight Line"
@@ -44,10 +39,37 @@ def drawPosition(frame, pos):
 def drawDisplay(frame):
     cv2.rectangle(frame, (5, 5), (505, 130), (60, 60, 60), -1)
 
+def drawlane(lane_img, color):
+    cv2.fillPoly(lane_img, np.int_([road]), color)
 
+def findLane():
+    #Threads and data for lane finding
+    left_line_thread = threading.Thread(target=left_line.find, args=(tframe,))
+    right_line_thread = threading.Thread(target=right_line.find, args=(tframe,))
 
-def drawlane(lane_img):
-    cv2.fillPoly(lane_img, np.int_([road]), (200, 0, 0))
+    left_line_thread.start()
+    right_line_thread.start()
+
+    left_line_thread.join()
+    right_line_thread.join()
+
+def displayInfo():
+    # Threads for information display
+    laneThread = threading.Thread(target=drawlane, args=(lane_img, color,))
+    displayThread = threading.Thread(target=drawDisplay, args=(frame,))
+    textThread = threading.Thread(target=drawCurvature,
+                                  args=(frame, left_line.getPolynomial()[0], right_line.getPolynomial()[0],))
+    posThread = threading.Thread(target=drawPosition, args=(frame, position,))
+
+    laneThread.start()
+    displayThread.start()
+    textThread.start()
+    posThread.start()
+
+    laneThread.join()
+    displayThread.join()
+    textThread.join()
+    posThread.join()
 
 
 if __name__ == '__main__':
@@ -57,82 +79,46 @@ if __name__ == '__main__':
     if not video.isOpened():
         print("Error opening video")
 
-
     ret, initial_frame = video.read()
-    cv2.imshow('tFrame', initial_frame)
     initial_frame = PerspectiveTransform.forward(initial_frame)
     left_starting_point, right_starting_point = LaneFinder.getStartingPoints(initial_frame)
 
-    i=1
-
     left_line = Lane.Lane(left_starting_point)
     right_line = Lane.Lane(right_starting_point)
+    left_line_fit = None
+    right_line_fit = None
     road = None
+    position = 0
+    color = (0, 0, 0)
 
     while video.isOpened():
         ret, frame = video.read()
         if ret:
-            start = time.time()
-
+            #start = time.time()
 
             tframe = PerspectiveTransform.forward(frame)
 
-            left_line_thread = threading.Thread(target=left_line.find, args=(tframe,))
-            right_line_thread = threading.Thread(target=right_line.find, args=(tframe,))
-
-            left_line_thread.start()
-            right_line_thread.start()
-
-            left_line_thread.join()
-            right_line_thread.join()
-
+            findLane();
             left_line_fit = left_line.getFit()
             right_line_fit = right_line.getFit()
 
             road = np.hstack(([left_line_fit], [np.flipud(right_line_fit)]))
 
-            lane_img = np.zeros_like(frame)
-
-            # lane_img = cv2.polylines(lane_img, [left_line.getFit()], False, (0, 155, 255), 10)
-            #lane_img = cv2.polylines(lane_img, [right_line.getFit()], False, (0, 155, 255), 10)
-
-            #print(left_line.getFit())
-
             position = (1280 // 2 - (right_line_fit[0][0] + left_line_fit[0][0]) // 2) * 3.7 / 700
 
+            color = (0, 200, 0)
+            if (abs(position) >= 0.3):
+                color = (0, 0, 200)
 
-
-
-
-            # Draw the lane onto the warped blank image
-            laneThread = threading.Thread(target=drawlane, args=(lane_img,))
-            displayThread = threading.Thread(target=drawDisplay, args=(frame,))
-            textThread = threading.Thread(target=drawCurvature, args=(frame, left_line.getPolynomial()[0], right_line.getPolynomial()[0],))
-            posThread = threading.Thread(target=drawPosition, args=(frame, position,))
-
-            laneThread.start()
-            displayThread.start()
-            textThread.start()
-            posThread.start()
-
-            laneThread.join()
-            displayThread.join()
-            textThread.join()
-            posThread.join()
-
-            tframe = PreProcessing.binarize(tframe)
-            #frame = PerspectiveTransform.backward(tframe)
-            #frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+            lane_img = np.zeros_like(frame)
+            displayInfo();
 
             lane_img = PerspectiveTransform.backward(lane_img)
             frame = cv2.addWeighted(frame, 1, lane_img, 0.5, 0)
 
-
-
             cv2.imshow('tFrame', frame)
 
-            #print(time.time()-start)
-
+            #print(time.time() - start)
 
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
@@ -141,4 +127,6 @@ if __name__ == '__main__':
 
     video.release()
     cv2.destroyAllWindows()
-
+    for thread in threading.enumerate():
+        if thread != threading.current_thread():
+            thread.join()
--- PreProcessing.py
+++ PreProcessing.py
@@ -1,142 +1,43 @@
 import numpy as np
 import cv2
-from numba import jit, cuda
 
-
-
-
-
-def gaussian_smoothing(image, sigma, w_kernel):
-    """ Blur and normalize input image.
-
-        Args:
-            image: Input image to be binarized
-            sigma: Standard deviation of the Gaussian distribution
-            w_kernel: Kernel aperture size
-
-        Returns:
-            binarized: Blurred image
-    """
-
-    # Define 1D kernel
-    s = sigma
-    w = w_kernel
-    kernel_1D = [np.exp(-z * z / (2 * s * s)) / np.sqrt(2 * np.pi * s * s) for z in range(-w, w + 1)]
-
-    # Apply distributive property of convolution
-    kernel_2D = np.outer(kernel_1D, kernel_1D)
-
-    # Blur image
-    smoothed_img = cv2.filter2D(image, cv2.CV_8U, kernel_2D)
-
-    # Normalize to [0 254] values
-    smoothed_norm = np.array(image.shape)
-    smoothed_norm = cv2.normalize(smoothed_img, None, 0, 255, cv2.NORM_MINMAX)
-
-    return smoothed_norm
-
-
+def color_correction(img):
+    corrected = cv2.convertScaleAbs(img, alpha=1.1, beta=-90)
+    corrected = cv2.cvtColor(corrected, cv2.COLOR_RGB2HSV)
+    corrected[:, :, 1] = np.zeros_like(corrected[:, :, 1])
+    corrected = cv2.cvtColor(corrected, cv2.COLOR_HSV2RGB)
+    return corrected
 def binarize(img):
-    #SEPARATE FUNCTION
-    binary = cv2.convertScaleAbs(img, alpha=1.1, beta=-90)
-    binary = cv2.cvtColor(binary, cv2.COLOR_RGB2HSV)
-    binary[:, :, 1] = np.zeros_like(binary[:, :, 1])
-    binary = cv2.cvtColor(binary, cv2.COLOR_HSV2RGB)
-
-    ret, binary = cv2.threshold(binary[:,:,0], int(cv2.mean(binary[:,:,0])[0])+45, 255, cv2.THRESH_BINARY)
-
+    binary = color_correction(img)
     img_lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
     img_hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
 
+    ret, binary = cv2.threshold(binary[:,:,0], int(cv2.mean(binary[:,:,0])[0])+45, 255, cv2.THRESH_BINARY)
     binary_img = np.zeros_like(binary)
     binary_img[(img_lab[:, :, 2] > 155) | (binary > 1) ] = 255
 
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 31))
+    mask1 = cv2.morphologyEx(binary_img, cv2.MORPH_TOPHAT, kernel)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 7))
-    black = cv2.morphologyEx(img_lab[:, :, 0], cv2.MORPH_TOPHAT, kernel)
-    lanes = cv2.morphologyEx(img_hls[:, :, 1], cv2.MORPH_TOPHAT, kernel)
+    mask2 = cv2.morphologyEx(img_hls[:, :, 1], cv2.MORPH_TOPHAT, kernel)
+    mask3 = cv2.morphologyEx(img_lab[:, :, 0], cv2.MORPH_TOPHAT, kernel)
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 31))
-    yellow = cv2.morphologyEx(binary_img, cv2.MORPH_TOPHAT, kernel)
+    return combine_masks(binary, mask1, mask2, mask3)
 
-    #lanes = np.zeros_like(lanes)
-    #black = np.zeros_like(black)
-    #yellow = np.zeros_like(yellow)
-    final_mask = np.zeros_like(binary)
-    final_mask[(yellow > 1) | (lanes > 2) | (black > 3)] = 255
+def combine_masks(img, mask1, mask2, mask3):
+    final_mask = np.zeros_like(img)
+    final_mask[(mask1 > 1) | (mask2 > 2) | (mask3 > 3)] = 255
 
-    small_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 7))
-    final_mask = cv2.morphologyEx(final_mask, cv2.MORPH_ERODE, small_kernel, iterations=2)
-    small_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    final_mask = cv2.morphologyEx(final_mask, cv2.MORPH_ERODE, small_kernel, 10)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 7))
+    final_mask = cv2.morphologyEx(final_mask, cv2.MORPH_ERODE, kernel, iterations=2)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    final_mask = cv2.morphologyEx(final_mask, cv2.MORPH_ERODE, kernel, 10)
 
-    small_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 31))
-    final_mask = cv2.morphologyEx(final_mask, cv2.MORPH_DILATE, small_kernel)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 31))
+    final_mask = cv2.morphologyEx(final_mask, cv2.MORPH_DILATE, kernel)
 
     return final_mask
-
-
-def binarize_kmeans(image, it):
-    """ Binarize an image using k-means.
-
-        Args:
-            image: Input image
-            it: K-means iteration
-    """
-
-    # Set random seed for centroids
-    # image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
-    cv2.setRNGSeed(124)
-
-    # Flatten image
-    flattened_img = image.reshape((-1, 1))
-    flattened_img = np.float32(flattened_img)
-
-    # Set epsilon
-    epsilon = 0.2
-
-    # Estabish stopping criteria (either `it` iterations or moving less than `epsilon`)
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, it, epsilon)
-
-    # Set K parameter (2 for thresholding)
-    K = 2
-
-    # Call kmeans using random initial position for centroids
-    _, label, center = cv2.kmeans(flattened_img, K, None, criteria, it, cv2.KMEANS_RANDOM_CENTERS)
-
-    # Colour resultant labels
-    center = np.uint8(center)  # Get center coordinates as unsigned integers
-    # print(center[0], center[1])
-    if(center[1] - center[0]) < 20:
-        center[0] = 0
-        center[1] = 0
-    else:
-        center[0] = 0
-        center[1] = 255
-
-    flattened_img = center[label.flatten()]  # Get the color (center) assigned to each pixel
-
-    # Reshape vector image to original shape
-    binarized = flattened_img.reshape((image.shape))
-
-    # binarized = cv2.cvtColor(binarized, cv2.COLOR_GRAY2RGB)
-
-    # Show resultant image
-    # final = np.zeros_like(img)
-    # final[binarized > 10] = 255
-
-    return binarized
-
-
-def Canny(img):
-    #img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    img = cv2.Canny(img, 100, 200, apertureSize=3)
-    #img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-
-
-    return img
 
 def bilateral_filter(img):
     return cv2.bilateralFilter(img, 5, 25, 25)
